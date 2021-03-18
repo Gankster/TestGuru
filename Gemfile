0d07392ff155a3d704d7ad891b9c273c454d998b
Andrii Boreiko <andriyboreyko@gmail.com>
2021-03-18 11:09:57 +0200

    add js

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'rails-i18n', '~> 6.0'
 
 gem 'jquery-rails'
 gem 'bootstrap', '~> 5.0.0.beta2'
+gem "octicons_helper"
 
 gem 'octokit', '~> 4.20'
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("../utilities/sorting")
+require("../utilities/password_identification")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
