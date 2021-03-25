689eaf63349cbe8c4d097faa4d2fae7d4a830ad8
Andrii Boreiko <andriyboreyko@gmail.com>
2021-03-25 09:54:04 +0200

    upgrade gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: "fierce-lowlands-97934.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "testguru-rails.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
